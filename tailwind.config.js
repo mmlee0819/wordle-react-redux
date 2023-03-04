c9e677b55294dc536524a883769de020ce727d16
mmlee0819 <mmlee0819@gmail.com>
2023-03-04 10:40:58 +0800

    Add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       screens: {
         xs: { max: "370px" },
+        maxMd: { max: "768px" },
       },
       borderColor: {
         gloomy: "#3a3a3c",
@@ -16,6 +17,13 @@ module.exports = {
         wrong: "#3a3a3c",
         wrongSpot: "#b59f3b",
         correct: "#538d4e",
+      },
+      fontSize: {
+        "2.5xl": ["28px", "40px"],
+        "3.5xl": [
+          "32px",
+          { lineHeight: "65px", letterSpacing: "0.01em", fontWeight: "700" },
+        ],
       },
     },
   },
