604bd7d5c05c32beb5c00819b42f4296cfdfb715
mmlee0819 <mmlee0819@gmail.com>
2023-03-15 16:47:14 +0800

    Add feature of log-out

--- src/components/profile/form/partition.tsx
+++ src/components/profile/form/partition.tsx
@@ -1,6 +1,6 @@
 export default function Partition() {
   return (
-    <div className="flex w-full items-center justify-between">
+    <div className="flex w-full items-center justify-between select-none">
       <span className="my-8 w-5/12 border-white border-b-2"></span>
       <p>or</p>
       <span className="my-8 w-5/12 border-white border-b-2"></span>
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,42 +1,14 @@
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth"
-import { db, auth, provider } from "../../../lib/firebase"
+import { useSelector } from "react-redux"
+import { RootStateType } from "@/store/reducers"
 import Form from "@/components/profile/form"
-import Button from "@/components/profile/button"
-import Partition from "@/components/profile/partition"
-import Image from "next/dist/client/image"
-
-const googleButtonStyles =
-  "flex justify-center items-center mt-4 w-full h-12 text-white bg-black rounded border border-white font-semibold select-none gap-2"
+import UserInfo from "@/components/profile/userInfo"
 
 export default function Profile() {
-  const handleClickGoogle = async (e: React.MouseEvent) => {
-    try {
-      const result = await signInWithPopup(auth, provider)
-      const credential = GoogleAuthProvider.credentialFromResult(result)
-      const token = credential?.accessToken
-      console.log({ result })
-      console.log({ credential })
-      console.log({ token })
-    } catch (error) {
-      if (error instanceof Error) {
-        const errorMessage = error.message
-        console.log({ errorMessage })
-      }
-    }
-  }
+  const userState = useSelector((state: RootStateType) => state.userReducer)
+  const { isAuthenticated } = userState
   return (
-    <div className="flex flex-col justify-center mt-8 mx-auto max-w-md">
-      <Form />
-      <Partition />
-      <Button onClick={handleClickGoogle} styles={googleButtonStyles}>
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-          alt="google logo"
-          width={20}
-          height={20}
-        />
-        Continue with Google
-      </Button>
+    <div className="flex flex-col justify-center items-center mt-8 mx-auto max-w-md">
+      {isAuthenticated ? <UserInfo /> : <Form />}
     </div>
   )
 }
